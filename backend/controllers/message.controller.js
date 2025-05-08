a22cd3f6b758b0dd2bf2eb7571388b79ded87efa
Ali <aliprg0@gmail.com>
2025-05-08 18:22:28 +0330

    add message controllers , routes , models

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,53 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      text: message,
+    });
+
+    conversation.messages.push(newMessage._id);
+    await Promise.all([conversation.save(), newMessage.save()]);
+  } catch (error) {
+    console.error("Error in sendMessage controller:", error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const userId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [userId, userToChatId] },
+    }).populate("messages");
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error in getMessage controller:", error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import AuthRoutes from "./routes/auth.route.js";
+import MessageRoutes from "./routes/message.route.js";
 import connectDB from "./db/mongodb.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
-app.use("/api/auth", AuthRoutes);
 
+app.use("/api/auth", AuthRoutes);
+app.use("api/message", MessageRoutes);
 app.listen(PORT, () => {
   connectDB();
   console.log("Server is running on port 5000");
