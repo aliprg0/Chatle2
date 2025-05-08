1b48c9f490343a1494e2d5f70afa6bfa0dd5a2ff
Ali <aliprg0@gmail.com>
2025-05-08 16:48:38 +0330

    add signup controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,48 @@
-export const signup = (req, res) => {
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      res.status(400).json({ error: "Passwords do not match." });
+    }
+
+    const user = await User.findOne({ username });
+    if (user) {
+      res.status(400).json({ error: "The username has already been taken." });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    // The await newUser.save() will throw an error if data is invalid based on schema,
+    // which will be caught by the catch block.
+    await newUser.save();
+
+    // If save is successful, then generate token and send response
+    generateTokenAndSetCookie(newUser._id, res);
+    res.status(201).json({
+      // Consider returning only necessary user info, not the hashed password
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profilePic,
+      gender: newUser.gender,
+    });
   } catch (error) {
     console.error("Error in signup:", error.message);
     res.status(500).json({ message: "Internal server error" });
