6ad6bfcc834808d9196774603b6a2af21231dcdb
Ali <aliprg0@gmail.com>
2025-05-08 16:09:17 +0330

    add user model and add express middleware for reading json

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import AuthRoutes from "./routes/auth.route.js";
 import connectDB from "./db/mongodb.js";
 
 dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
 app.use("/api/auth", AuthRoutes);
 
 app.listen(PORT, () => {
