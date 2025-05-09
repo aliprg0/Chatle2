a2caa1a1aa6c8a3d6e0c44d4fe906da798833685
Ali <aliprg0@gmail.com>
2025-05-09 14:55:06 +0330

    modifed online status css

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -25,7 +25,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
         onClick={() => setSelectedConversation(conversation)}
       >
         <div className="relative">
-          <div className={`avatar ${isOnline ? "online" : "offline"}`}>
+          <div className={`avatar ${isOnline ? "online" : ""}`}>
             <div className="w-12 rounded-full">
               <img src={conversation.profilePic} alt="user avatar" />
             </div>
