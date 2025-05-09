b77e24f59f8e8edb0f2c84a378d8be28153da1c0
Ali <aliprg0@gmail.com>
2025-05-09 16:32:21 +0330

    fix port duplication error

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-<<<<<<< HEAD
-=======
 import cors from "cors";
 import path from "path";
->>>>>>> main
 
 import AuthRoutes from "./routes/auth.route.js";
 import MessageRoutes from "./routes/message.route.js";
@@ -14,10 +11,7 @@ import connectDB from "./db/mongodb.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
-<<<<<<< HEAD
 const PORT = process.env.PORT || 5000;
-=======
->>>>>>> main
 
 const corsOptions = {
   origin: ["http://localhost:3000", "https://chatle2.vercel.app"],
@@ -34,16 +28,11 @@ app.use("/api/auth", AuthRoutes);
 app.use("/api/message", MessageRoutes);
 app.use("/api/user", UserRoutes);
 
-<<<<<<< HEAD
-=======
 // Serve static files in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(process.cwd(), "frontend", "dist")));
 }
 
-const PORT = process.env.PORT || 5000;
-
->>>>>>> main
 server.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
